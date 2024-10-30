c5c3cacf46b46277a5d2098e22e93eb92855b7ac
Israel Oluwajuwon Adewuyi <i.adewuyi@innopolis.university>
2024-10-30 15:49:03 +0300

    feat: :construction: added transcript link

---
+++ config/transcript_link.py
@@ -0,0 +1,5 @@
+"""List of websites containing Dwarkesh podcast transcripts to be used as ground truth in RAG"""
+
+PODCAST_LINKS = [
+    "https://www.dwarkeshpatel.com/p/sholto-douglas-trenton-bricken?open=false#%C2%A7transcript"
+]
